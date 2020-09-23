681319afb1a1f390491d67c659eec3c2c706e589
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-09-23 17:48:46 +0200

    Add constructor to User model

--- src/main/java/com/example/animalshelter/model/User.java
+++ src/main/java/com/example/animalshelter/model/User.java
@@ -1,4 +1,8 @@
-package model;
+package com.example.animalshelter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,4 +24,9 @@ public class User {
 
   @Column(length = 64, nullable = false)
   private String password;
+
+  public User(String name, String password) {
+    this.name = name;
+    this.password = password;
+  }
 }
