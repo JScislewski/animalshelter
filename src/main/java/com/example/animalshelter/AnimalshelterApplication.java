0466f76c7ccaccb98cbc99e55915c37114d45ae2
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-09-23 16:24:22 +0200

    Correct style

--- src/main/java/com/example/animalshelter/AnimalshelterApplication.java
+++ src/main/java/com/example/animalshelter/AnimalshelterApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnimalshelterApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AnimalshelterApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(AnimalshelterApplication.class, args);
+  }
 }
