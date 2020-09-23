1c0746a0b7fa8a386fdac08f1847205fdba3deab
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-09-23 18:28:17 +0200

    Add sample data and controller

--- src/main/java/com/example/animalshelter/controller/UserController.java
+++ src/main/java/com/example/animalshelter/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.example.animalshelter.controller;
 
 import com.example.animalshelter.model.User;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@RestController
 @RequestMapping("/api")
 public class UserController {
-  @GetMapping("/admin/candidates")
+
+  @GetMapping("/users")
   public ResponseEntity<List<User>> getAllUsers() {
     List<User> users = new ArrayList<>();
     User user1 = new User("Jan", "password");
