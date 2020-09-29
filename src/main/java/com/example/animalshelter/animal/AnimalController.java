b3c1d1e3f63206625fb20a5fb7e8bd3f08191b30
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-09-29 16:37:51 +0200

    Add getting animal by id

--- src/main/java/com/example/animalshelter/animal/AnimalController.java
+++ src/main/java/com/example/animalshelter/animal/AnimalController.java
@@ -1,10 +1,12 @@
 package com.example.animalshelter.animal;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +21,13 @@ public class AnimalController {
   @GetMapping("/animal")
   public ResponseEntity<List<Animal>> getAllAnimals() {
     return new ResponseEntity<>(animalRepository.findAll(), HttpStatus.OK);
+  }
+
+  @GetMapping("/animal")
+  public ResponseEntity<Animal> getAnimal(@RequestParam(name = "id") long id) {
+    Optional<Animal> animal = animalRepository.findById(id);
+    return animal
+      .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+      .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 }
